32f933d5f3321118c547612ba2767cac5bd76514
FlaReis <flavia.reis@squadra.com.br>
2025-10-25 23:24:42 -0300

    feat: compile routes and integrate express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import productsRoutes from "./product-routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    endpoints: [
+      'POST /api/products',
+    ]
+  });
+});
+
+app.use('/api', productsRoutes);
 
 export default app;
