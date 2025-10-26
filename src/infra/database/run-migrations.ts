0576b0815875ec0ba40f92611711ebde32c62c05
FlaReis <flavia.reis@squadra.com.br>
2025-10-26 18:37:44 -0300

    feat: include upPromotions in run-migrations.ts

--- src/infra/database/run-migrations.ts
+++ src/infra/database/run-migrations.ts
@@ -1,9 +1,11 @@
 import { up as upProducts } from "./migrations/001-create-products_table";
+import { up as upPromotions } from "./migrations/002-create-promotions_table";
 
 
 async function runMigrations() {
   try {
     await upProducts();
+    await upPromotions();
     console.log('Migration executed successfully');
     process.exit(0);
   } catch (err) {
