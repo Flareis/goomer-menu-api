a527fb46a5f60562101e04e597b2953a95b88a2b
FlaReis <flavia.reis@squadra.com.br>
2025-10-26 23:24:53 -0300

    feat: udes regex to validate hour format

--- src/services/promotions.service.ts
+++ src/services/promotions.service.ts
@@ -1,10 +1,20 @@
 import { Promotions } from '../entities/promotions.entity';
 import { PromotionsRepository } from '../repositories/promotions.repository';
 
+// uso de regex para verificar a formatação HH:mm
+const TIME_REGEX = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
+// isValidTime recebe uma string e retorna true se estiver no formato HH:mm
+function isValidTime(time: string) {
+  return TIME_REGEX.test(time);
+}
 export class PromotionsService {
   constructor(private repository = new PromotionsRepository()) { }
 
   async create(promotion: Omit<Promotions, 'id'>): Promise<Promotions> {
+    if (!isValidTime(promotion.start_time) || !isValidTime(promotion.end_time)) {
+      throw new Error('Invalid time format. Please use the HH:mm format.');
+    }
     return this.repository.create(promotion);
   }
 
