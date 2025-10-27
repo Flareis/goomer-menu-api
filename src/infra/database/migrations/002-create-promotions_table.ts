20d91b97db93a883cf68adcfaf5da2c02a0b4522
FlaReis <flavia.reis@squadra.com.br>
2025-10-26 23:01:35 -0300

    feat: exclude category and include product_id

--- src/infra/database/migrations/002-create-promotions_table.ts
+++ src/infra/database/migrations/002-create-promotions_table.ts
@@ -4,12 +4,13 @@ export async function up() {
   await db.query(`
      CREATE TABLE IF NOT EXISTS promotions (
         id INT AUTO_INCREMENT PRIMARY KEY,
+        product_id INT NOT NULL,
         description_promotion VARCHAR(255) NOT NULL,
         price DECIMAL(10,2) NOT NULL,
-        category VARCHAR(255),
         week_days SET('mon','tue','wed','thu','fri','sat','sun'),
         start_time TIME,
-        end_time TIME
+        end_time TIME,
+        CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
       );
   `);
   console.log("Table created successfully!");
