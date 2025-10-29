29bc74340a79ac6ef124d91b4487b994a8bd3a2b
Flareis <reis.dos.flavia@gmail.com>
2025-10-29 00:09:17 -0300

    feat: compile routes and integrate express

---
+++ src/routes/menu-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { MenuController } from "../controllers/menu.controller";
+
+const menuRoutes = Router();
+const menuController = new MenuController();
+
+menuRoutes.get('/', (req, res) =>
+  menuController.getMenu(req, res))
+
+export default menuRoutes;
