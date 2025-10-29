928cdb8e401d9d9089e8b674f843f11a48c97b5f
Flareis <reis.dos.flavia@gmail.com>
2025-10-29 17:01:10 -0300

    feat: add validation to the interval between appointments

--- src/services/promotions.service.ts
+++ src/services/promotions.service.ts
@@ -12,9 +12,18 @@ export class PromotionsService {
   constructor(private repository = new PromotionsRepository()) { }
 
   async create(promotion: Omit<Promotions, 'id'>): Promise<Promotions> {
+    const start = new Date(`1970-01-01T${promotion.start_time}:00`);
+    const end = new Date(`1970-01-01T${promotion.end_time}:00`);
+    const diffMinutes = (end.getTime() - start.getTime()) / (1000 * 60);
+
+    if (diffMinutes < 15) {
+      throw new Error('The interval between appointments should be at least 15 minutes.');
+    }
+
     if (!isValidTime(promotion.start_time) || !isValidTime(promotion.end_time)) {
       throw new Error('Invalid time format. Please use the HH:mm format.');
     }
+
     return this.repository.create(promotion);
   }
 
